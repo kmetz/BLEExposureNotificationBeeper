ddcb645529b30b0e607864d5c54772b71a2a8e40
Kaspar Metz <k@kjpm.de>
2020-06-19 18:03:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# BLE Exposure Notification Beeper
+
+Allows you to build a simple device that beeps and flashes an LED whenever someone using a COVID-19 warning/tracing app that uses [Bluetooth LE Exposure Notifications](https://en.wikipedia.org/wiki/Exposure_Notification), like the german [Corona-Warn-App](https://www.coronawarn.app/en/), is seen nearby.
+
+Notifiers (nearby devices with a warning app) are remembered for 20 minutes so it only beeps for newly detected ones. Note that the IDs will change every 15 minutes or so for privacy reasons, triggering new beeps.
+
+## Build your own
+### You need
+1. Any ESP32 board (like a WEMOS D32 or TTGO ESP32, preferrably with a LiPo battery controller).
+2. A 3V piezo buzzer (other buzzer types might also work).
+3. Optionally a LED.
+4. Optionally a small 1-cell LiPo battery.
+5. A nice case :)
+
+### Wiring
+
+| ESP32 pin | goes to             |
+|:----------|:--------------------|
+| 0         | LED (+)             |
+| 1         | Buzzer (+)          |
+| GND       | LED (-), Buzzer (-) |
+
+- Pins can easily be changed in code.
+
+### Flashing the ESP32
+- Easiest is to upload the latest binary, using [NodeMCU PyFlasher
+](https://github.com/marcelstoer/nodemcu-pyflasher).
+- If you have [platform.io](https://platformio.org/platformio-ide) installed, simply open the project and upload.
+  - You also need the [arduino-esp32](https://github.com/espressif/arduino-esp32) platform and the [lbernstone/Tone](https://github.com/lbernstone/Tone) platform. 
---
+++ src/main.cpp
@@ -0,0 +1,113 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+#include "Tone32.h"
+
+
+// Pin where the + of an LED is connected.
+#define LED_PIN 0
+
+// Pin where the + of a piezo buzzer is connected.
+#define BUZZER_PIN 2
+
+// Tone to play.
+#define BEEP_NOTE NOTE_C6
+
+// Tone duration in milliseconds.
+#define BEEP_DURATION_MS 100
+
+// Scan update time, 5 seems to be a good value.
+#define SCAN_TIME_SECONDS 5
+
+// When to forget old senders.
+#define FORGET_AFTER_MINUTES 20
+
+
+BLEScan *scanner;
+std::map<std::string, unsigned long> seenNotifiers;
+
+
+/**
+ * Called when a new exposure notifier is seen.
+ */
+void onNewNotifierFound() {
+  Serial.println("BEEP");
+  digitalWrite(LED_PIN, HIGH);
+  tone(BUZZER_PIN, BEEP_NOTE, BEEP_DURATION_MS, 0);
+  digitalWrite(LED_PIN, LOW);
+  sleep(BEEP_DURATION_MS);
+}
+
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  /**
+   * Called when a BLE device is discovered.
+   */
+  void onResult(BLEAdvertisedDevice advertisedDevice) override {
+    if (!advertisedDevice.haveServiceUUID()
+        || advertisedDevice.getServiceDataUUID().toString().substr(0, 9) != "0000fd6f-") {
+      return;
+    }
+
+    if (!seenNotifiers.count(advertisedDevice.getAddress().toString())) {
+      // New notifier found.
+      Serial.printf("+   %s \r\n", advertisedDevice.getAddress().toString().c_str());
+      onNewNotifierFound();
+    }
+    else {
+      // We've already seen this one.
+      Serial.printf("... %s \r\n", advertisedDevice.getAddress().toString().c_str());
+    }
+
+    // Remember, with time.
+    seenNotifiers[advertisedDevice.getAddress().toString()] = millis();
+  }
+};
+
+
+/**
+ * Remove notifiers last seen over FORGET_AFTER_MINUTES ago.
+ */
+void forgetOldNotifiers() {
+  for (auto const &notifier : seenNotifiers) {
+    if (notifier.second + (FORGET_AFTER_MINUTES * 60 * 1000) < millis()) {
+      Serial.printf("-   %s \r\n", notifier.first.c_str());
+      seenNotifiers.erase(notifier.first);
+    }
+  }
+}
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hi.");
+
+  // Initialize pins.
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  // Initialize scanner.
+  BLEDevice::init("ESP");
+  scanner = BLEDevice::getScan();
+  scanner->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  scanner->setActiveScan(true); // Active scan uses more power, but gets results faster.
+  scanner->setInterval(100);
+  scanner->setWindow(99);
+
+  Serial.println("Scanning ...");
+}
+
+
+void loop() {
+  Serial.println("-----");
+
+  // Scan.
+  scanner->start(SCAN_TIME_SECONDS, false);
+
+  // Cleanup.
+  scanner->clearResults();
+  forgetOldNotifiers();
+
+  Serial.printf("Count: %d \r\n", seenNotifiers.size());
+}
